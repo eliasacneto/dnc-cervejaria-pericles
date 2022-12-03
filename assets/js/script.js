796c2d52bb5713b34992ff51260c1a2f5d1796b8
netoeac <netoeac.dev@gmail.com>
2022-12-03 12:36:39 -0300

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+function goToPage(value){
+  if(value ==1){
+    window.location.href="allowed.html"  
+  } else { 
+    window.location.href="not-allowed.html"  
+  }
+  
+}
+
+function goHome(){
+  window.location.href="welcome.html"
+}
